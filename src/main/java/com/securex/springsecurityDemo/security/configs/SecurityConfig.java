8fd6c8d3456746272d93983346362a236d9593bd
James M W <jimmymugo1@gmail.com>
2024-01-03 18:53:55 +0300

    InMemoryUserDetailsManager-Approach1

--- src/main/java/com/securex/springsecurityDemo/security/configs/SecurityConfig.java
+++ src/main/java/com/securex/springsecurityDemo/security/configs/SecurityConfig.java
@@ -7,6 +7,9 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,4 +25,27 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    /*
+    * InMemoryUserDetailsManager -- Demo ---  not recommended for Production.
+    * */
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin,user);
+
+    }
+
 }
