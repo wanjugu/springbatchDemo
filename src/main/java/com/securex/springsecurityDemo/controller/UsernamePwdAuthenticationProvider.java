fbdce5b572c9f9bdf208b3adc2174c098c8a192a
James M W <jimmymugo1@gmail.com>
2024-01-05 17:48:44 +0300

    Custom Authentication Provider

---
+++ src/main/java/com/securex/springsecurityDemo/controller/UsernamePwdAuthenticationProvider.java
@@ -0,0 +1,57 @@
+package com.securex.springsecurityDemo.controller;
+
+import com.securex.springsecurityDemo.entities.Customer;
+import com.securex.springsecurityDemo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
+public class UsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        List<Customer> customer = customerRepository.findByEmail(username);
+        if(customer.size() > 0){
+            if(passwordEncoder.matches(password, customer.get(0).getPassword())){
+                List<GrantedAuthority> authorities = new ArrayList<>();
+                authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username,password,authorities);
+
+            }else{
+                throw new BadCredentialsException("Invalid username and or password.");
+            }
+
+        }else{
+
+            throw new BadCredentialsException("User not found.");
+
+        }
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
--- src/main/java/com/securex/springsecurityDemo/security/configs/Users.java
+++ src/main/java/com/securex/springsecurityDemo/security/configs/Users.java
@@ -1,41 +1,41 @@
-package com.securex.springsecurityDemo.security.configs;
-
-import com.securex.springsecurityDemo.entities.Customer;
-import com.securex.springsecurityDemo.repository.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-@Service
-public class Users implements UserDetailsService {
-    @Autowired
-    CustomerRepository customerRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        String userName, password = null;
-        List<GrantedAuthority> authorities = null;
-        List<Customer> customer = customerRepository.findByEmail(username);
-
-        if(customer.size() == 0){
-            throw new UsernameNotFoundException("Userdetails not found for user: "+ username);
-        }else{
-            userName = customer.get(0).getEmail();
-            password = customer.get(0).getPassword();
-            authorities = new ArrayList<>();
-            authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
-
-        }
-        return new User(username,password,authorities);
-    }
-}
+//package com.securex.springsecurityDemo.security.configs;
+//
+//import com.securex.springsecurityDemo.entities.Customer;
+//import com.securex.springsecurityDemo.repository.CustomerRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//
+//@Service
+//public class Users implements UserDetailsService {
+//    @Autowired
+//    CustomerRepository customerRepository;
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//
+//        String userName, password = null;
+//        List<GrantedAuthority> authorities = null;
+//        List<Customer> customer = customerRepository.findByEmail(username);
+//
+//        if(customer.size() == 0){
+//            throw new UsernameNotFoundException("Userdetails not found for user: "+ username);
+//        }else{
+//            userName = customer.get(0).getEmail();
+//            password = customer.get(0).getPassword();
+//            authorities = new ArrayList<>();
+//            authorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+//
+//        }
+//        return new User(username,password,authorities);
+//    }
+//}
