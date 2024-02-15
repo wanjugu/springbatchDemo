0262ad719462046855995feb837bedfc7c29599f
James M W <jimmymugo1@gmail.com>
2024-02-15 10:01:19 +0300

    Add Csrf filter

--- src/main/java/com/securex/springsecurityDemo/security/configs/SecurityConfig.java
+++ src/main/java/com/securex/springsecurityDemo/security/configs/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.securex.springsecurityDemo.security.configs;
 
 
+import com.securex.springsecurityDemo.security.configs.filter.CsrfCookieFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.core.userdetails.User;
@@ -17,6 +19,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -26,11 +31,22 @@ import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {
+
+    };
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf"); //You can set your own, here we've used the default valu3 from the class
+
+//        http.securityContext().requireExplicitSave(false)
 
         http.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(configurationSource()))
-                .csrf(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable) //TODO how to configure for csrf
+                .csrf((csrf)->csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers( "/contact","/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())).addFilter(new CsrfCookieFilter())
+
                 .authorizeHttpRequests( (requests)-> requests
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards","/user").authenticated()
                 .requestMatchers("/notices", "/contact","/register","/upload").permitAll())
@@ -39,7 +55,6 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 
 
     /*
